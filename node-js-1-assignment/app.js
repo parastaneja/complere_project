8fd7bb230533a4396647275440edf74593831842
Paras <paras.taneja@complereinfosystem.com>
2021-08-19 11:24:06 +0530

    Eslint enabled for the project

--- node-js-1-assignment/app.js
+++ node-js-1-assignment/app.js
@@ -1,9 +1,9 @@
 const express = require('express');
+const logger = require('./libs/logger')();
 
 const app = express();
 const PORT = 7000;
 
 app.get('/', (req, res) => res.send('Hello Sid'));
 
-
-app.listen(PORT, () => console.log(`server is running on port ${PORT}`));
+app.listen(PORT, () => logger.info(`My server is running on port ${PORT}`));
