4ac48444f08e34980639d6a00dc50a760bd1d39c
Paras <paras.taneja@complereinfosystem.com>
2021-08-20 11:01:25 +0530

    Modified logs

--- node-js-1-assignment/app.js
+++ node-js-1-assignment/app.js
@@ -6,4 +6,4 @@ const PORT = 7000;
 
 app.get('/', (req, res) => res.send('Hello Sid'));
 
-app.listen(PORT, () => logger.info(`My server is running on port ${PORT}`));
+app.listen(PORT, () => logger.info(`First express server running on ${PORT}`));
