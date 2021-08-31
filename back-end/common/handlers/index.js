27a849c5768e838a71a21623dd744b82e4f1e166
Paras Taneja <paras_v@remitr.com>
2021-08-31 17:15:18 +0530

    Added error handler and utils

--- back-end/common/handlers/index.js
+++ back-end/common/handlers/index.js
@@ -1,5 +1,7 @@
 const successHandler = require('./success.handler');
+const errorHandler = require('./error.handler');
 
 module.exports = {
   successHandler,
+  errorHandler,
 };
--- back-end/index.routes.js
+++ back-end/index.routes.js
@@ -1,9 +1,19 @@
 const express = require('express');
+const { errorUtils } = require('./common/utils');
+const logger = require('./libs/logger')();
 
 const { employeeRoutes } = require('./employees');
 
 const apiRoutes = express.Router({ mergeParams: true });
 
 apiRoutes.use('/employees', employeeRoutes);
+
+apiRoutes.use('*', (req, res, next) => {
+  try {
+    return errorUtils.throwNotFound('Route');
+  } catch (e) {
+    return next(e);
+  }
+});
 
 module.exports = apiRoutes;
