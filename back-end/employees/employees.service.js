65b8a9e6d7bb69d8456ad65b5bdafc68710d2eee
Paras Taneja <paras_v@remitr.com>
2021-09-01 18:02:31 +0530

    Covered validation, Managers and env variables

--- back-end/employees/employees.service.js
+++ back-end/employees/employees.service.js
@@ -1,21 +1,19 @@
-const employeeList = [{
-  employee_id: 1,
-  employee_name: 'Kajal',
-},
-{
-  employee_id: 2,
-  employee_name: 'Rajan',
-},
-{
-  employee_id: 3,
-  employee_name: 'Siddharth',
-},
-{
-  employee_id: 4,
-  employee_name: 'Adarsh',
-}];
+const { pgManager } = require('../managers');
+const config = require('../config');
+const logger = require('../libs/logger')();
 
-const getEmployeeList = async () => employeeList;
+const getEmployeeList = async () => {
+  const conn = await pgManager.getConnection();
+  const query = `
+    SELECT * FROM ${config.pgsql.schema}.employee order by employee_id desc
+    `;
+  logger.info({
+    function_name: 'getEmployeeList',
+    query,
+  });
+  const { rows } = await conn.query(query);
+  return rows;
+};
 
 module.exports = {
   getEmployeeList,
---
+++ back-end/managers/index.js
@@ -0,0 +1,10 @@
+const {
+  PgManager,
+} = require('../libs/managers');
+const config = require('../config');
+
+const pgManager = PgManager(config.pgsql);
+
+module.exports = {
+  pgManager,
+};
